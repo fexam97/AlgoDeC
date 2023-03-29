a5ef26e80cff0381614478c260f72d84e43b0966
Fernando Meza <70779182+fexam97@users.noreply.github.com>
2023-03-29 12:23:34 -0300

    Update main.c

--- tp1/Ejercicio3/main.c
+++ tp1/Ejercicio3/main.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-#define EULER  2.7182818284
-#define TOL 0.00001
 #include <math.h>
 
+#define EULER  2.7182818284
+#define TOL 0.00001
+
 float factorial(float dato);
+float numeroEuler(void);
 
 int main()
 {
-    double e;
+
     int potencia;
-    int i = 0; // iterador
     float numero;
-
-    e = 0; // e = x^0 + e^1
-
-    while( 1 / factorial(i) > TOL){
-
-        e += 1 / factorial(i);
-        i++;
-    }
 
     printf("\n\n ingrese la potencia: ");
     scanf("%d", &potencia);
 
-    numero = pow(e, potencia);
+    numero = pow(numeroEuler(), potencia);
 
     printf("\n  e^%d = %.10f ", potencia, numero);
     printf("\n aproximadamente : %.10f ", pow(EULER, potencia));
@@ -41,8 +34,19 @@ float factorial(float dato) {
         for(int i = 1 ; i <= dato ; i++)
             acum = acum * i;
     }
-
     return acum;
-
 }
 
+float numeroEuler(void) {
+
+    int i = 0;
+    float e = 0; // e = x^0 + e^1
+
+     while( 1 / factorial(i) > TOL){
+
+        e += 1 / factorial(i);
+        i++;
+    }
+
+    return e;
+}
