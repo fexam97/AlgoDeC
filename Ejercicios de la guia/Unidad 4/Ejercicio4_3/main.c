72d829026962f419463606b756f413dfe3ce7db3
Fernando Meza <fexam97@gmail.com>
2023-06-07 14:40:34 -0300

    sumar fecha

---
+++ Ejercicios de la guia/Unidad 4/Ejercicio4_3/main.c
@@ -0,0 +1,17 @@
+#include "funciones.h"
+
+int main()
+{
+    tFecha fechaUno = {5, 5, 2001};
+    int numeroAsumar;
+    printf("La fecha es : %d/%d/%d", fechaUno.dia, fechaUno.mes, fechaUno.anio);
+
+
+    //printf("\n ingrese un numero a sumar :");
+    //scanf("%d", &numeroAsumar);
+
+    sumarFecha(fechaUno, 4867);
+
+
+    return 0;
+}
